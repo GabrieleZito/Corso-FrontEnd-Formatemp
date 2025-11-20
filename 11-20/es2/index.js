5f0ba3176b27383153b59f390d5ff1889be61d8d
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2025-11-20 17:15:06 +0100

    Es. Indovina il colore

---
+++ 11-20/es2/index.js
@@ -0,0 +1,33 @@
+const colors = ["red", "aqua", "green", "black", "blue", "blueviolet", "orange", "gray", "yellow"];
+const secret = colors[Math.floor(Math.random() * colors.length)];
+console.log(secret);
+
+const colorDiv = document.querySelector(".colors");
+
+const msg = document.querySelector("#messaggio");
+
+colors.forEach((c) => {
+    const div = document.createElement("div");
+    div.style.height = "100px";
+    div.style.width = "100px";
+    div.style.backgroundColor = c;
+    div.classList.add("color");
+
+    div.addEventListener("click", () => {
+        const isSecret = c == secret ? true : false;
+        if (isSecret) {
+            msg.textContent = "🎉 Hai indovinato il colore segreto!";
+            const divs = document.querySelectorAll(".color");
+            divs.forEach((d) => {
+                d.style.backgroundColor = secret;
+            });
+        } else {
+            msg.textContent = "😅 Riprova!";
+            const altri = colors.filter((col) => col != secret && col != c);
+            msg.textContent = `😅 Riprova! 
+             Hint: Non è neanche il ${altri[Math.floor(Math.random() * altri.length)]}`;
+        }
+    });
+
+    colorDiv.appendChild(div);
+});
